32a83c4a8893cafbc8085e2eca589b0392687f2a
BinadaH <65110730+BinadaH@users.noreply.github.com>
2026-07-03 20:57:38 +0200

    better export + 4.7

--- scripts/utils/GenerateExport.cs
+++ scripts/utils/GenerateExport.cs
@@ -2,6 +2,7 @@ using CSharpMath.SkiaSharp;
 using Godot;
 using Godot.Collections;
 using SkiaSharp;
+using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 
@@ -71,12 +72,12 @@ public partial class GenerateExport : Node
                 for (int i = 0; i < page_objs.Count; i++)
                 {
                     CanvasItem[] objs = page_objs[i].ToArray<CanvasItem>();
-                    Vector2 position = page_positions[i];
+                    Vector2 page_position = page_positions[i];
                     Vector2I size = page_sizes[i];
 
                     using (var canvas = document.BeginPage(size.X, size.Y))
                     {
-                        HandleDraw(canvas, objs, position, size);
+                        HandleDraw(canvas, objs, page_position, size);
                     }
                     document.EndPage();
                     
@@ -92,31 +93,31 @@ public partial class GenerateExport : Node
         }
     }
 
-    private void HandleDraw(SKCanvas canvas, CanvasItem[] objs, Vector2 position, Vector2I size) 
+    private void HandleDraw(SKCanvas canvas, CanvasItem[] objs, Vector2 page_position, Vector2I size) 
     {
         using (var paint = new SKPaint())
         {
             DrawBackground(canvas, paint, size);
             if (grid_on)
-                DrawGrid(canvas, paint, position, size);
+                DrawGrid(canvas, paint, page_position, size);
 
 
             foreach (var item in objs)
             {
                 if (item is Line2D line)
                 {
-                    DrawLine2D(line, position, paint, canvas);
+                    DrawLine2D(line, page_position, paint, canvas);
                 }
                 else if (item.IsInGroup("text"))
                 {
-                    DrawText((Control)item, position, paint, canvas);
+                    DrawText((Control)item, page_position, paint, canvas);
                 }
             }
         }
     }
 
 
-    private void DrawText(Control item, Vector2 position, SKPaint paint, SKCanvas canvas)
+    private void DrawText(Control item, Vector2 page_position, SKPaint paint, SKCanvas canvas)
     {
         var text_lines = item.Call("get_line_nodes").AsGodotArray<Node>();
         var latex_blocks = item.Get("latex_blocks").AsStringArray();
@@ -133,7 +134,7 @@ public partial class GenerateExport : Node
 
                     string txt = label.Text;
                     var obj_rect = label.GetGlobalRect();
-                    var pos = obj_rect.Position - position;
+                    var pos = obj_rect.Position - page_position;
                     var rect_height = obj_rect.Size.Y;
                     using (var font = new SKFont(fontTypeface, font_size))
                     {
@@ -143,7 +144,7 @@ public partial class GenerateExport : Node
                 }
                 else if (text_block is Godot.TextureRect texture_rect)
                 {
-                    var pos = texture_rect.GetGlobalRect().Position - position;
+                    var pos = texture_rect.GetGlobalRect().Position - page_position;
                     var painter = new MathPainter();
                     var expr = latex_blocks[latex_i];
                     painter.LaTeX = expr;
@@ -159,7 +160,7 @@ public partial class GenerateExport : Node
         }
     }
 
-    private void DrawLine2D(Line2D line, Vector2 position, SKPaint paint, SKCanvas canvas)
+    private void DrawLine2D(Line2D line, Vector2 page_position, SKPaint paint, SKCanvas canvas)
     {
         paint.Style = SKPaintStyle.Stroke;
         paint.IsAntialias = true;
@@ -171,48 +172,35 @@ public partial class GenerateExport : Node
                                 (byte)(line.DefaultColor.B * 255),
                                 (byte)(line.DefaultColor.A * 255)
                             );
-        using (var path = new SKPath())
+        if (line.Points.Length > 0)
         {
-            if (line.Points.Length > 0)
+            Vector2[] points = line.Points;
+            int count = points.Length;
+
+            if (count < 2) return;
+
+            float baseWidth = line.Width;
+            Curve widthCurve = line.WidthCurve;
+
+            Vector2 offsetTotal = line.Position - page_position;
+
+            for (int i = 0; i < count - 1; i++)
             {
-                Curve curve = line.WidthCurve;
-                float maxSegmentLength = 3.0f;
+                Vector2 p1Shifted = points[i] + offsetTotal;
+                Vector2 p2Shifted = points[i + 1] + offsetTotal;
 
-                for (int i = 0; i < line.Points.Length - 1; i++)
-                {
-                    Vector2 translated_p1 = line.Points[i] + line.Position - position;
-                    Vector2 translated_p2 = line.Points[i + 1] + line.Position - position;
+                float t1 = (float)i / (count - 1);
+                float t2 = (float)(i + 1) / (count - 1);
+                float w1 = baseWidth * (widthCurve != null ? widthCurve.Sample(t1) : 1f);
+                float w2 = baseWidth * (widthCurve != null ? widthCurve.Sample(t2) : 1f);
 
-                    float distance = translated_p1.DistanceTo(translated_p2);
+                float currentWidth = (w1 + w2) / 2f;
+                paint.StrokeWidth = currentWidth;
 
-                    int substeps = Mathf.Max(1, Mathf.CeilToInt(distance / maxSegmentLength));
-
-                    float tStart = (float)i / (line.Points.Length - 1);
-                    float tEnd = (float)(i + 1) / (line.Points.Length - 1);
-
-                    Vector2 currentSubP1 = translated_p1;
-
-                    for (int j = 1; j <= substeps; j++)
-                    {
-                        float subT = (float)j / substeps;
-
-                        Vector2 currentSubP2 = translated_p1.Lerp(translated_p2, subT);
-
-                        float globalT = Mathf.Lerp(tStart, tEnd, subT);
-
-                        SKPoint skP1 = new SKPoint(currentSubP1.X, currentSubP1.Y);
-                        SKPoint skP2 = new SKPoint(currentSubP2.X, currentSubP2.Y);
-
-                        float curveFactor = (curve != null) ? curve.SampleBaked(globalT) : 1.0f;
-                        paint.StrokeWidth = curveFactor * line.Width;
-
-                        canvas.DrawLine(skP1, skP2, paint);
-
-                        currentSubP1 = currentSubP2;
-                    }
-                }
+                canvas.DrawLine(p1Shifted.X, p1Shifted.Y, p2Shifted.X, p2Shifted.Y, paint);
             }
         }
+
     }
 
     private void DrawBackground(SKCanvas canvas, SKPaint paint, Vector2I size)
@@ -222,14 +210,14 @@ public partial class GenerateExport : Node
         canvas.DrawRect(new SKRect(0, 0, size.X, size.Y), paint);
     }
 
-    private void DrawGrid(SKCanvas canvas, SKPaint paint, Vector2 position, Vector2I size)
+    private void DrawGrid(SKCanvas canvas, SKPaint paint, Vector2 page_position, Vector2I size)
     {
         paint.Style = SKPaintStyle.Stroke;
         paint.StrokeWidth = grid_w;
         paint.Color = GDColor2SKColor(grid_color);
 
-        float offset_x = (float)(Godot.Mathf.Floor(position.X / sq_size) + 1) * sq_size - position.X;
-        float offset_y = (float)(Godot.Mathf.Floor(position.Y / sq_size) + 1) * sq_size - position.Y;
+        float offset_x = (float)(Godot.Mathf.Floor(page_position.X / sq_size) + 1) * sq_size - page_position.X;
+        float offset_y = (float)(Godot.Mathf.Floor(page_position.Y / sq_size) + 1) * sq_size - page_position.Y;
         int num_x = (int)(size.X / sq_size);
         int num_y = (int)(size.Y / sq_size);
 
